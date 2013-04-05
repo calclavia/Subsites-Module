7e0055b4aac4d7733aa39eee483b1d07906dcc8b
Henry Mao <admin@calclavia.com>
2013-04-05 15:12:19 +0800

    Reformat

--- subsites.js
+++ subsites.js
@@ -1,36 +1,34 @@
-(function ($) {
+(function($) {
 
-Drupal.behaviors.subsites_visibility = function() {
-  var pages = $('#edit-pages-wrapper');
-  var subsites_visibility = $('#advanced-visibility');
-  var visibility = $("input[name='visibility']:checked").val();
+	Drupal.behaviors.subsites_visibility = function() {
+		var pages = $('#edit-pages-wrapper');
+		var subsites_visibility = $('#advanced-visibility');
+		var visibility = $("input[name='visibility']:checked").val();
 
-  if (visibility >= 3) {
-    pages.hide();
-  }
-  else {
-    subsites_visibility.hide();
-  }
+		if (visibility >= 3) {
+			pages.hide();
+		} else {
+			subsites_visibility.hide();
+		}
 
-  $("input[name='visibility']").change(function() {
-    var visibility = $("input[name='visibility']:checked").val();
-    if (visibility >= 3) {
-      pages.hide();
-      subsites_visibility.show();
-    }
-    else {
-      subsites_visibility.hide();
-      pages.show();
-    }
-  });
-};
+		$("input[name='visibility']").change(function() {
+			var visibility = $("input[name='visibility']:checked").val();
+			if (visibility >= 3) {
+				pages.hide();
+				subsites_visibility.show();
+			} else {
+				subsites_visibility.hide();
+				pages.show();
+			}
+		});
+	};
 
-Drupal.behaviors.vertical_tabs_subsiteFieldsetSummary = {
-  attach: function (context) {
-    $('fieldset#edit-subsites', context).drupalSetSummary(function (context) {
-      return Drupal.checkPlain($('#edit-subsite option:selected', context).text());
-    });
-  }
-};
+	Drupal.behaviors.vertical_tabs_subsiteFieldsetSummary = {
+		attach : function(context) {
+			$('fieldset#edit-subsites', context).drupalSetSummary(function(context) {
+				return Drupal.checkPlain($('#edit-subsite option:selected', context).text());
+			});
+		}
+	};
 
-})(jQuery);
+})(jQuery); 
